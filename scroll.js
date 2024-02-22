66ec7047ad75786e3a98f14682397b9cb8bf8919
SupRMinecraftien <superminecraftien.levrai@gmail.com>
2024-02-22 20:36:28 +0100

    Initial Commit

---
+++ scroll.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("html").easeScroll({
+        frameRate: 60,
+        animationTime: 1000,
+        stepSize: 120,
+        pulseAlgorithm: 1,
+        pulseScale: 8,
+        pulseNormalize: 1,
+        accelerationDelta: 20,
+        accelerationMax: 1,
+        keyboardSupport: true,
+        arrowScroll: 50,
+        touchpadSupport: true,
+        fixedBackground: true
+    });
+});
